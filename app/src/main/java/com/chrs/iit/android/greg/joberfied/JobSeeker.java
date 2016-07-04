1f7e104c941e282ef1164313e0e8592c41561c9e
GoogleMoGreg <jgfuntera.chrs@gmail.com>
2016-07-05 00:08:46 +0800

    Added Employer Login

---
+++ app/src/main/java/com/chrs/iit/android/greg/joberfied/JobSeeker.java
@@ -0,0 +1,13 @@
+package com.chrs.iit.android.greg.joberfied;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobSeeker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_seeker);
+    }
+}
