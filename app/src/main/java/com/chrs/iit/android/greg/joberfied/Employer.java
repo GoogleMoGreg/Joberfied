1dc032b9754f5d5cb9b7a3e3778039ba6788329c
GoogleMoGreg <jgfuntera.chrs@gmail.com>
2016-07-19 07:32:27 +0800

    Updated Registration for JobSeeker and Added Registration UI for Employer

--- app/src/main/java/com/chrs/iit/android/greg/joberfied/Employer.java
+++ app/src/main/java/com/chrs/iit/android/greg/joberfied/Employer.java
@@ -1,6 +1,7 @@
 package com.chrs.iit.android.greg.joberfied;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +10,17 @@ import android.widget.Button;
 public class Employer extends AppCompatActivity implements View.OnClickListener{
 
 
+    Button btn_login,btn_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer);
 
 
+        btn_login=(Button)findViewById(R.id.loginEmployer_btn);
+        btn_login.setOnClickListener(this);
+        btn_signup=(Button)findViewById(R.id.signupEmployer_btn);
+        btn_signup.setOnClickListener(this);
     }
 
     @Override
@@ -25,10 +31,20 @@ public class Employer extends AppCompatActivity implements View.OnClickListener{
             case R.id.loginEmployer_btn:
 
                 break;
+
+            case R.id.signupEmployer_btn:
+                 ShowEmployerRegister();
+                break;
+
         }
     }
     public void showEmployerMain(){
 
+    }
 
+    public void ShowEmployerRegister(){
+
+        Intent showEmployerRegister=new Intent(this,Employer_Register.class);
+        startActivity(showEmployerRegister);
     }
 }
---
+++ app/src/main/java/com/chrs/iit/android/greg/joberfied/Employer_Register.java
@@ -0,0 +1,15 @@
+package com.chrs.iit.android.greg.joberfied;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Employer_Register extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer__register);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
